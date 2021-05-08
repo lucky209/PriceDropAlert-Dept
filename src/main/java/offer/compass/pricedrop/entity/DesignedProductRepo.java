76e228c36afb708547503e2ed40bc8aefc615015
athirsta <athirsta.raja@promon.co.in>
2021-05-08 11:29:50 +0530

    small fix while deleting the products

--- src/main/java/offer/compass/pricedrop/entity/DesignedProductRepo.java
+++ src/main/java/offer/compass/pricedrop/entity/DesignedProductRepo.java
@@ -1,6 +1,7 @@
 package offer.compass.pricedrop.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import javax.transaction.Transactional;
@@ -12,6 +13,7 @@ public interface DesignedProductRepo extends JpaRepository<DesignedProduct, Stri
 
     @Query(value = "delete from pricedropalert.designed_product where created_date <= ?1", nativeQuery = true)
     @Transactional
+    @Modifying
     void deleteRecordsByCreatedDate(LocalDate date);
 
     @Query(value = "select count(site_url) from pricedropalert.designed_product where created_date <= ?1", nativeQuery = true)
