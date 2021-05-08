d4bb9734dc54aba440f1447e3813fc1f2d69e83a
athirsta <athirsta.raja@promon.co.in>
2021-05-08 18:52:38 +0530

    lowest price ever findings changes reverted

--- src/main/java/offer/compass/pricedrop/constant/PropertyConstants.java
+++ src/main/java/offer/compass/pricedrop/constant/PropertyConstants.java
@@ -7,5 +7,4 @@ public class PropertyConstants {
     public static final String PRODUCTS_SAVED_IN_LAST_ATTEMPT_COUNT = "products.saved.in.last.attempt.count";
     public static final String MAX_THREADS_COUNT = "max.threads.count";
     public static final String FILTER_FACTOR_THRESHOLD = "filter.factor.threshold.value";
-    public static final String LOWEST_PRICE_EVER = "lowest.price.ever";
 }
